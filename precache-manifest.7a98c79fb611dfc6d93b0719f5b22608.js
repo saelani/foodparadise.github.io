d5c325fefd36a9b6a2002549420d7c12b555490b
saelani <saelani@gmail.com>
2020-06-26 08:01:17 +0700

    paradise

---
+++ precache-manifest.7a98c79fb611dfc6d93b0719f5b22608.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46a416c8e404f369a6067973ae732907",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c7037009deee459026c3",
+    "url": "/static/css/main.b342fe4d.chunk.css"
+  },
+  {
+    "revision": "943193dfd5fd22149958",
+    "url": "/static/js/2.cac94360.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.cac94360.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7037009deee459026c3",
+    "url": "/static/js/main.a6a5bc7a.chunk.js"
+  },
+  {
+    "revision": "3f01166689480e3788ad",
+    "url": "/static/js/runtime-main.ebc9dc89.js"
+  },
+  {
+    "revision": "87b5f04a82a6dc765fcfebba13b6d9c8",
+    "url": "/static/media/aneka-makanan.87b5f04a.png"
+  }
+]);
